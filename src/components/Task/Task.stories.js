5d44a962c5c8034dc63c7c97564fadb8fac8fe1b
aidan-mcbride <aidanmcbride@outlook.com>
2020-02-29 09:31:28 -0700

    Add knobs addon
    https://www.learnstorybook.com/intro-to-storybook/vue/en/using-addons/

--- src/components/Task/Task.stories.js
+++ src/components/Task/Task.stories.js
@@ -1,12 +1,16 @@
 import { action } from '@storybook/addon-actions';
+import { withKnobs, object } from '@storybook/addon-knobs';
+
 import Task from './Task';
 
 import { taskData } from './testData';
+
 export default {
   title: 'Task',
-  // Our exports that end in "Data" are not stories.
+  decorators: [withKnobs],
   excludeStories: /.*Data$/
 };
+
 export const actionsData = {
   onPinTask: action('onPinTask'),
   onArchiveTask: action('onArchiveTask')
@@ -14,13 +18,15 @@ export const actionsData = {
 
 const taskTemplate = `<task :task="task" @archiveTask="onArchiveTask" @pinTask="onPinTask"/>`;
 
+const longTitle = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
 // default task state
 export const Default = () => ({
   components: { Task },
   template: taskTemplate,
   props: {
     task: {
-      default: () => taskData
+      default: object('task', { ...taskData })
     }
   },
   methods: actionsData
@@ -49,6 +55,20 @@ export const Archived = () => ({
         ...taskData,
         state: 'TASK_ARCHIVED'
       }
+    }
+  },
+  methods: actionsData
+});
+// default task state with long title
+export const LongTitle = () => ({
+  components: { Task },
+  template: taskTemplate,
+  props: {
+    task: {
+      default: () => ({
+        ...taskData,
+        title: longTitle
+      })
     }
   },
   methods: actionsData
